1abbbbbc8c6e42d595028b09efed0363b2ba570c
umer <syedumerahmedcode@gmail.com>
2026-02-22 15:58:33 +0500

    Feature: Implemented the solution and tested it against a valid test case scenario.

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public boolean isHappy(int n) {
-
+        // This is used to detect if cycles are present
+        Set<Integer> seenNumbers = new HashSet<>();
+        while (n != 1 && !seenNumbers.contains(n)) {
+            seenNumbers.add(n);
+            n = getSumOfSquares(n);
+        }
+        return n == 1;
     }
 
+    private int getSumOfSquares(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
 }
